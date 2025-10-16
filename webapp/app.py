e142588f41a777bbe0fd2eb5354d556cd47b05a9
Lance Lui Lam <lancekyao@gmail.com>
2025-10-16 23:44:53 +1100

    Add other important bits

--- webapp/app.py
+++ webapp/app.py
@@ -13,16 +13,40 @@ towns = location_this_file / "static/towns.txt"
 with open(towns, "r") as f:
     towns = f.read()
 
-towns = towns.split("\n")
-print(towns[0])
 
+towns_info = {}
+
+for town in towns.split("\n"):
+    towns_info[town] = town.lower().replace(", ", "-").replace(" ", "-")
+
+def sanitise_str(to_sanitise):
+    return to_sanitise.translate(str.maketrans({
+    "-":  r"\-",
+    "]":  r"\]",
+    "\\": r"\\",
+    "^":  r"\^",
+    "$":  r"\$",
+    "*":  r"\*",
+    ".":  r"\."
+}))
 
 @app.route("/")
 def index():
     logger.info("Connected to index")
-    return render_template("index.jinja2", towns=towns)
+    return render_template("index.jinja2", towns=towns_info)
 
-@app.route("/test")
-def test():
-    logger.info("Connected to test")
-    return "<p>Test!</p>"
+# TODO: when user presses "go", it should redirect the user to the relevant town page containing 
+# the town information
+
+@app.route("/town/<town_url>")
+def town_info(town_url):
+    town_url = sanitise_str(town_url)
+    if town_url not in town_info.keys():
+        logger.warning(f"Could not find {town_url}")
+        return render_template("notfound.jinja2", towns=list(towns_info.keys()))
+    logger.info(f"Connected to {town_url}")
+    return render_template("town.jinja2", towns=list(towns_info.keys()))
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("notfound.jinja2", towns=list(towns_info.keys()))

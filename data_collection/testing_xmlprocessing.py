5781f35a8eac06263c0642ab8bb1bffaf3fa88a2
Lance Lui Lam <lancekyao@gmail.com>
2025-10-16 21:48:57 +1100

    Testing multiple files where information could be found

--- data_collection/testing_xmlprocessing.py
+++ data_collection/testing_xmlprocessing.py
@@ -2,13 +2,16 @@ import xml.etree.ElementTree as ET
 from pathlib import Path
 from fuzzysearch import find_near_matches
 
-from datetime import date, timedelta, datetime
+from datetime import datetime
+import logging
+
+logger = logging.getLogger(__name__)
 
 files = ["./data_collection/downloads/IDQ11295.xml", "./data_collection/downloads/IDQ10230.xml"]
 
-file = "./data_collection/downloads/IDQ11295.xml"
+# file = "./data_collection/downloads/IDQ11295.xml"
 
-query = "Beaudesert"
+query = "Stanthorpe"
 query = query.translate(str.maketrans({
     "-":  r"\-",
     "]":  r"\]",
@@ -19,36 +22,41 @@ query = query.translate(str.maketrans({
     ".":  r"\."
 }))
 
-with open(file, "r") as f:
-    tree = ET.parse(file)
+values = {}
 
-root_main = tree.getroot()[1] # 0th element is amoc
+for file in files:
 
-result = root_main.findall(f".area[@description='{query}'][@type='location']")
+    with open(file, "r") as f:
+        tree = ET.parse(file)
 
+    root_main = tree.getroot()[1] # 0th element is amoc
 
-if len(result[0]) != 8:
-    print("error: what?")
-    breakpoint()
+    result = root_main.findall(f".area[@description='{query}'][@type='location']")
 
-d1 = {}
-d0 = {}
+    if not result:
+        logger.warning(f"File {file[-12:]} could not find results for {query}, skipping")
+        continue
+    if len(result[0]) != 8:
+        logger.warning(f"File {file[-12:]} does not have all the required information, skipping")
+        continue
 
-for forecast in result[0]:
-    # Within each element, extract the information type, as well as the information
-    # Stored in its own dictionary
-    for element in forecast:
-        print(f"{element.attrib['type']} : {element.text}")
-        d0[element.attrib['type']] = element.text
-    breakpoint()
-
-    _date = datetime.fromisoformat(forecast.attrib["start-time-local"]).strftime(r"%Y-%m-%d")
-
-    d1[_date] = d0
+    d1 = {}
     d0 = {}
 
+    for forecast in result[0]:
+        # Within each element, extract the information type, as well as the information
+        # Stored in its own dictionary
+        for element in forecast:
+            print(f"{element.attrib['type']} : {element.text}")
+            d0[element.attrib['type']] = element.text
+
+        _date = datetime.fromisoformat(forecast.attrib["start-time-local"]).strftime(r"%Y-%m-%d")
+
+        d1[_date] = d0
+        d0 = {}
+    values[file[-12:]] = d1
+
 breakpoint()
-    
     
 
 

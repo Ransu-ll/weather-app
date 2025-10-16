78a786f18597c90de66bcdc7998db4f617b79856
Lance Lui Lam <lancekyao@gmail.com>
2025-10-16 23:00:56 +1100

    Allow for searching of towns

--- webapp/app.py
+++ webapp/app.py
@@ -1,14 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
 import logging
+from pathlib import Path
 
 logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
 
+location_this_file = Path(__file__).parent.resolve()
+
+towns = location_this_file / "static/towns.txt"
+
+with open(towns, "r") as f:
+    towns = f.read()
+
+towns = towns.split("\n")
+print(towns[0])
+
+
 @app.route("/")
 def index():
     logger.info("Connected to index")
-    return "<p>Hello, World!</p>"
+    return render_template("index.jinja2", towns=towns)
 
 @app.route("/test")
 def test():
